a38b903c1a96494348bf8d73261355f38d2084e6
0ygtgns <0gunesyigit@gmail.com>
2025-04-13 19:53:30 +0300

    bişilerdenedimdeolmadıdaha
    swordscriptini gerçekten firlatmaliyapmayacalısıyom

--- Scripts/sword.gd
+++ Scripts/sword.gd
@@ -1,19 +1,16 @@
 extends RigidBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var character_node = get_node_or_null("character")
-
-func _ready() -> void:
-	pass 
+@onready var character_node = get_node("character")
 
 
 func _process(delta: float) -> void:
 	var animated_sprite = character_node.get_node("AnimatedSprite2D")
 	var is_sword_active_character = Input.is_action_just_pressed("Character_Sword")
-	if is_sword_active_character and animated_sprite.flip.h == 0:
+	if is_sword_active_character and animated_sprite.flip_h== 0:
 		visible = true
 		animation_player.play("sword_flip")
 		linear_velocity.x = 350
-	elif is_sword_active_character and animated_sprite.flip.h == 1:
+	elif is_sword_active_character and animated_sprite.flip_v == 1:
 		visible = true
 		animation_player.play("sword_flip")
 		linear_velocity.x = -350
