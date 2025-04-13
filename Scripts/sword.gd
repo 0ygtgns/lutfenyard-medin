d372c34a8718871a3bf95d2282ece8b23d88c6a7
0ygtgns <0gunesyigit@gmail.com>
2025-04-13 18:47:47 +0300

    gravityswordof

--- Scripts/sword.gd
+++ Scripts/sword.gd
@@ -1,11 +1,10 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var is_sword_active = Input.is_action_just_pressed("Character_Sword")
+	if is_sword_active:
+		var linear_velocity = 130
