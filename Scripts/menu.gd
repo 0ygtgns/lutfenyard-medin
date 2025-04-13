b801656002579f22cb227e71794ed75143858a57
huseyinremzikara <hsynrmzkr@gmail.com>
2025-04-13 19:35:56 +0300

    settings_is_been_developing

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
 
 func _on_quit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
