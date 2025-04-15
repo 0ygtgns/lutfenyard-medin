5e1754c10ac524aecc7d8d25e02397f76daeb33e
huseyinremzikara <hsynrmzkr@gmail.com>
2025-04-16 01:09:54 +0300

    pause_added

--- Scripts/paused_menu.gd
+++ Scripts/paused_menu.gd
@@ -1,16 +1,33 @@
 extends Control
 
+func resume():
+	get_tree().paused = false
+	hide()
+func pause():
+	get_tree().paused = true
+	show()
 
-# Called when the node enters the scene tree for the first time.
+func testesc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused== false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
 func _ready() -> void:
-	pass # Replace with function body.
+	get_tree().paused = false
+	hide()
 
 func _on_resume_pressed() -> void:
-	pass # Replace with function body.
+	resume()
 
 func _on_restart_pressed() -> void:
+	hide()
 	get_tree().reload_current_scene()
 
-
 func _on_quit_pressed() -> void:
+	get_tree().paused= false
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+func _process(delta: float) -> void:
+	testesc()
+	
--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -6,7 +6,7 @@ func _on_volume_value_changed(value: float) -> void:
 
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_volume_db(0, toggled_on)
+	AudioServer.set_bus_mute(0, toggled_on)
 
 
 
