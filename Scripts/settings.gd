3acc6ea221f2e82cba9ea8200687b52631414902
huseyinremzikara <hsynrmzkr@gmail.com>
2025-04-15 23:01:57 +0300

    pause ekliom

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -6,14 +6,23 @@ func _on_volume_value_changed(value: float) -> void:
 
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(0,toggled_on)
+	AudioServer.set_bus_volume_db(0, toggled_on)
 
 
-func _on_resolutions_item_selected(index: int) -> void:
+
+
+func _on_resolutions_2_item_selected(index: int) -> void:
 	match index:
 		0:
 			DisplayServer.window_set_size(Vector2i(1920,1080))
 		1:
 			DisplayServer.window_set_size(Vector2i(1600,900))
 		2:
-			DisplayServer.window_set_size(Vector2i(1280, 720))
+			DisplayServer.window_set_size(Vector2i(1280,720))
+
+
+func _input(event):
+	if event.is_action_pressed("jump"):
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+		
+		
