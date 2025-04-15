6eb0162caa6ddd0f2e1fd6290513993ce0f2575e
huseyinremzikara <hsynrmzkr@gmail.com>
2025-04-15 23:09:12 +0300

    quit eklendi

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -22,7 +22,7 @@ func _on_resolutions_2_item_selected(index: int) -> void:
 
 
 func _input(event):
-	if event.is_action_pressed("jump"):
+	if event.is_action_pressed("esc"):
 		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 		
 		
